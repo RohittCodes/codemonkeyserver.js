90bfe6de7b5ea75def8731925e72a12ed27cc008
Rohith Singh <singhmalik583@gmail.com>
2024-05-30 21:50:40 +0530

    fixed cors

--- routes/codeChimp.js
+++ routes/codeChimp.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { getExplanation } from "../controllers/codeChimp.js";
+import cors from "cors";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+router.use(cors());
 
 router.post("/chat", getExplanation);
 
--- routes/codeTools.js
+++ routes/codeTools.js
@@ -3,9 +3,13 @@ import { debugCode, helpUser, optimizeCode } from "../controllers/codeTools.js";
 
 const router = express.Router();
 
+import cors from "cors";
+
 router.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+router.use(cors());
 
 router.post("/optimize", optimizeCode);
 router.post("/help", helpUser);
