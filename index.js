e0861a7602fb14ef6634982abc913c8c275c0f84
Rohith Singh <singhmalik583@gmail.com>
2024-05-30 20:48:16 +0530

    added corsOptions

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import { codeToolsRoute } from "./routes/codeTools.js";
 import { codeChimpRoute } from "./routes/codeChimp.js";
 import { problemRoute } from "./routes/problemRoute.js";
 
 dotenv.config();
+
+const corsOptions = {
+  origin: "https://codemonkey-js.vercel.app", // Change this to the client URL
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGODB_URI;
@@ -13,6 +20,18 @@ const MONGO_URI = process.env.MONGODB_URI;
 const app = express();
 
 app.use(express.json());
+app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://codemonkey-js.vercel.app"); // Change this to the client URL
+  res.header("Access-Control-Allow-Headers", true);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  next();
+});
 
 mongoose.connect(MONGO_URI);
 
